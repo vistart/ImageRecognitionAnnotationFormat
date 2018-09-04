30321ac782ee76049d78247b9899e95be299763c
vistart <i@vistart.me>
2018-09-04 15:38:43 +0800

    添加项目文件。

---
+++ ImageRecognitionAnnotationFormat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageRecognitionAnnotationFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
